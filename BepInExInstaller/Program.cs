a83e17610913102a582dabaa691a6b6e459bc198
Conoxxdll <darealgoldy@gmail.com>
2026-08-31 14:25:16 +0200

    Create Program.cs

---
+++ BepInExInstaller/Program.cs
@@ -0,0 +1,12 @@
+namespace BepInExInstaller
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
